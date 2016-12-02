d7ae64f3b6af06d425209f2a44fae0b0654caa38
Alex Kessock <alex.kessock@workday.com>
2016-12-01 22:43:37 -0700

    update header

--- src/app/actionMenu.jsx
+++ src/app/actionMenu.jsx
@@ -12,13 +12,13 @@ class ActionMenu extends React.Component {
     let text = null
     switch (state.selectedPlayers.length) {
       case 0:
-        text = 'Select 2 players to begin'
+        text = 'Select 2 players to begin.'
         break
       case 1:
         text = 'Select 1 more player!'
         break
       case 2:
-        text = 'Click here to play a game!'
+        text = 'Click here to play a game!!!'
         break
       default:
         text = "That's too many players! Select 2 to begin a game."
@@ -29,7 +29,7 @@ class ActionMenu extends React.Component {
         onClick={this._handleClick.bind(this)}
         className={state.selectedPlayers.length === 2 ? 'ready' : null}
       >
-        <h3>{text}</h3>
+        {text}
       </header>
     )
   }
