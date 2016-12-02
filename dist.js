ea44ce18a6fc4c401971295e8a2786257274ba49
Alex Kessock <alex.kessock@workday.com>
2016-12-01 20:42:40 -0700

    added a dist script

---
+++ dist.js
@@ -0,0 +1,16 @@
+const execSync = require('child_process').execSync
+
+console.log('Building bundle with webpack')
+execSync('node_modules/.bin/webpack --bail')
+
+execSync('rm -r dist')
+
+execSync('mkdir dist')
+execSync('cp src/index.html dist/')
+execSync('cp src/bundle.js dist/')
+
+execSync('mkdir dist/app')
+execSync('cp src/app/mountain.json dist/app/')
+execSync('cp src/app/king-pong-logo.png dist/app/')
+
+console.log('Dist build finished!')
