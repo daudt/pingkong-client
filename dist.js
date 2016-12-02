feb42fa0ac3841fa516932214d1df3d1557ad914
Alex Kessock <alex.kessock@workday.com>
2016-12-02 08:47:03 -0700

    add checkmark to dist script

--- dist.js
+++ dist.js
@@ -10,6 +10,7 @@ execSync('cp src/index.html dist/')
 execSync('cp src/bundle.js dist/')
 
 execSync('mkdir dist/app')
+execSync('cp src/app/checkmark.svg dist/app/')
 execSync('cp src/app/king-pong-logo.png dist/app/')
 execSync('cp src/app/king-pong-logo-wide.png dist/app/')
 
