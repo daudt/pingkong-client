f393013a74537d24199015fae5e2dab838aff2ff
ZenBlender <sbalay@gmail.com>
2016-12-22 20:07:32 -0700

    Log URL output.

--- src/app/loginPage.jsx
+++ src/app/loginPage.jsx
@@ -53,7 +53,8 @@ class LoginPage extends React.Component {
   _handleFacebookLogin() {
     // const url = `${Api.getBaseUrl()}/auth/facebook?auth_origin_url=${window.location.href}`
     const url = `${Api.getBaseUrl()}/auth/facebook?auth_origin_url=${encodeURIComponent('http://www.kingofpong.com/')}`
-    window.location.href = url
+    console.log('!!!', url)
+    // window.location.href = url
 
     // const email = document.querySelector('.loginEmail').value
     // const password = document.querySelector('.loginPassword').value
