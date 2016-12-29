09f4bc2eb731ef6d547ed22e289662a616e885b0
ZenBlender <sbalay@gmail.com>
2016-12-28 18:50:01 -0700

    Bust caches.

--- dist.js
+++ dist.js
@@ -9,6 +9,12 @@ execSync('mkdir dist')
 execSync('cp src/index.html dist/')
 execSync('mv src/bundle.js dist/')
 
+// Add cache-busting query param, but do some file juggling due to
+// inconsistencies in sed's in-place editing in OSX vs *nix:
+execSync("sed -e 's/bundle\\.js/bundle.js?r="+Math.random().toString(36)+"/g' dist/index.html > dist/index2.html")
+execSync('rm dist/index.html')
+execSync('mv dist/index2.html dist/index.html')
+
 execSync('mkdir dist/app')
 execSync('cp src/app/checkmark.svg dist/app/')
 execSync('cp src/app/king-pong-logo.png dist/app/')
