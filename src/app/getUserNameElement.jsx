2f6c444d414fee111a9a8069f3dd448e558fc66b
Alex Kessock <akessock@gmail.com>
2016-12-29 17:33:16 -0700

    users can now scroll to view the leaderboard as well as some mobile UI improvements

--- src/app/getUserNameElement.jsx
+++ src/app/getUserNameElement.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function getUserNameElement(user) {
   return (
-    <span className="userName">
+    <span className="user-name">
       {user.nickname || user.name}
       {user.nickname && user.name && user.nickname !== user.name ? <div className="subtle">{user.name}</div> : null}
     </span>
