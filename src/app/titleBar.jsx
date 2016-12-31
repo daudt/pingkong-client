71617c86f21b93bb2ef7b2acd4102cd0404be2ed
ZenBlender <sbalay@gmail.com>
2016-12-31 11:51:43 -0700

    Clickable title logo.

--- src/app/titleBar.jsx
+++ src/app/titleBar.jsx
@@ -1,16 +1,21 @@
 import React from 'react'
 
 import SessionControl from './sessionControl'
+import state from './state/'
 
 class TitleBar extends React.Component {
 
   render() {
     return (
       <div className="flex-column">
-        <img src="/app/king-pong-logo-wide.png" className="logo" />
+        <img src="/app/king-pong-logo-wide.png" className="logo" onClick={this._clickLogo.bind(this)} />
         <SessionControl />
       </div>
     )
+  }
+
+  _clickLogo() {
+    state.setPage('leaderboard')
   }
 
 }
